30ec1685d6c3bae87d8ca9afdf84c84ff4ee7099
kirkzhao <kirkzhao@tencent.com>
2024-06-05 19:39:54 +0800

    init

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ test:
 
 .PHONY: bin
 bin: fmt vet
-	go build -o bin/{{ .PluginName }} github.com/{{ .Owner }}/{{ .Repo }}/cmd/plugin
+	go build -o bin/kubectl-demo github.com/jiaolici/kubectl-demo/cmd/plugin
 
 .PHONY: fmt
 fmt:
--- cmd/plugin/cli/root.go
+++ cmd/plugin/cli/root.go
@@ -7,8 +7,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/{{ .Owner }}/{{ .Repo }}/pkg/logger"
-	"github.com/{{ .Owner }}/{{ .Repo }}/pkg/plugin"
+	"github.com/jiaolici/kubectl-demo/pkg/logger"
+	"github.com/jiaolici/kubectl-demo/pkg/plugin"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"github.com/tj/go-spin"
@@ -21,7 +21,7 @@ var (
 
 func RootCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:           "{{ .PluginName }}",
+		Use:           "kubectl-demo",
 		Short:         "",
 		Long:          `.`,
 		SilenceErrors: true,
